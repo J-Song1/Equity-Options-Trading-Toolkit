f36509fc77edfcb26dc16e9200689534e76696f4
Joon Song <joon.song21@gmail.com>
2022-08-20 11:48:30 -0400

    Updated get_otions_chain_data functions and added batch mode.

--- constants.py
+++ constants.py
@@ -5,10 +5,32 @@ Created: Aug 19, 2022
 Modified: Aug 19, 2022
 """
 
-# Exp. Aug 12, 2022 $400.00
-SPY_OPTIONS_BASE_URL = 'https://www.nasdaq.com/market-activity/funds-and-etfs/spy/option-chain/call-put-options/spy---'
+from datetime import date, timedelta
 
-# From Stackoverflow
+# Yahoo Finance Website Constants
+YFINANCE_URL = 'https://finance.yahoo.com'
+YFINANCE_TIMEOUT = 5.0
+DATE_QSTRING_REFERENCE = (
+    date(2022, 8, 22),
+    1661126400
+)
+
+# To "Emulate User Agent" - from Stackoverflow
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
 }
+
+# CBOE Constants
+CBOE_CLOSE = timedelta(hours=16) # 4:00 PM EST
+CBOE_HOLIDAYS = [
+    date(2022, 1, 17), # Martin Luther King, Jr. Day
+    date(2022, 2, 21), # Presidents' Day
+    date(2022, 4, 15), # Good Friday
+    date(2022, 5, 30), # Memorial Day
+    date(2022, 6, 20), # Juneteenth Holiday
+    date(2022, 7, 4),  # Independence Day
+    date(2022, 9, 5),  # Labor Day
+    date(2022, 11, 24), # Thanksgiving Day
+    date(2022, 11, 25), # Thanksgiving Early Close
+    date(2022, 12, 26), # Christmas Day Observed
+]
--- data.py
+++ data.py
@@ -1,37 +1,82 @@
+from re import L
 import requests
 import pandas as pd
+from datetime import datetime, date, timedelta
 from bs4 import BeautifulSoup
 
-from constants import HEADERS
+from constants import HEADERS, DATE_QSTRING_REFERENCE, YFINANCE_TIMEOUT, YFINANCE_URL
 
-# TODO: Add Caching Functionality
-def get_live_quote(
-    type='all',
-    strike: float=400.00,
-    expiry_year: int=22,
-    expiry_month: int=8,
-    expiry_day: int=12,
-    ):
-    assert type in ['all', 'call', 'put']
+# For Yahoo Finance
+def format_expiry_qstring(expiry: date) -> int:
+    ref_date, ref_qstring = DATE_QSTRING_REFERENCE
+    date_delta = expiry - ref_date
 
-    # Fetching Options Data from Yahoo Finance Webpage
-    response = requests.get(
-        'https://finance.yahoo.com/quote/SPY/options?date=1671148800',  #'https://finance.yahoo.com/quote/SPY/options', # TODO: Add Support for Static Dates
-        headers=HEADERS,
-        timeout=5 # TODO: Add Timeout Exception Handling
-    )
-    bsoup = BeautifulSoup(response.content, 'html.parser')
-    tables = bsoup.find_all('table')
+    expiry_qstring = ref_qstring + date_delta.days * 24 * 60 * 60 # Delta in Seconds
+    return expiry_qstring
 
-    # Options Data Cleaning
-    dfs = pd.read_html(str(tables))
-    for df in dfs:
-        df.set_index('Strike', inplace=True)
-        df['Volume'] = df['Volume'].replace('-', '0') .astype(int)
-        df['Implied Volatility'] = df['Implied Volatility'].str.replace('%', '') .astype(float) / 100.0
+def get_options_chain_data(expiry: date, ticker: str='SPY') -> tuple[pd.DataFrame, pd.DataFrame]:
+    assert ticker in ['SPY'] # Valid Ticker Assertion
 
-        # TODO: Add Option Expiry for Later
+    try:
+        response = requests.get(
+            f'{YFINANCE_URL}/quote/SPY/options?date={format_expiry_qstring(expiry)}',
+            headers=HEADERS,
+            timeout=YFINANCE_TIMEOUT,
+        )
+        bsoup = BeautifulSoup(response.content, 'html.parser')
+        tables = bsoup.find_all('table')
 
-    calls_df, puts_df = dfs
+        # Options Data Cleaning
+        dfs = pd.read_html(str(tables))
+        for df in dfs:
+            df['Bid'] = df['Bid'].replace('-', '0').astype(float)
+            df['Ask'] = df['Ask'].replace('-', '0').astype(float)
+            df['Volume'] = df['Volume'].replace('-', '0') .astype(int)
+            df['Expiry'] = datetime(expiry.year, expiry.month, expiry.day, 16)
+            df['Open Interest'] = df['Open Interest'].replace('-', '0').astype(int)
+            df['Implied Volatility'] = df['Implied Volatility'].str.replace('%', '') .astype(float) / 100.0
 
-    return calls_df, puts_df
+            df['Quote'] = datetime.now()
+            df.set_index(['Strike', 'Expiry'], inplace=True)
+
+        calls_df, puts_df = dfs
+        return calls_df, puts_df
+
+    except requests.exceptions.ReadTimeout:
+        return None
+
+def get_options_chain_data_batch(expiries: list[date], ticker: str='SPY') -> tuple[pd.DataFrame, pd.DataFrame]:
+    assert ticker in ['SPY'] # Valid Ticker Assertion
+
+    all_calls_df, all_puts_df = None, None
+    for expiry in expiries:
+        calls_df, puts_df = get_options_chain_data(expiry, ticker)
+        all_calls_df = pd.concat([all_calls_df, calls_df])
+        all_puts_df = pd.concat([all_puts_df, puts_df])
+        print(len(all_calls_df), len(all_puts_df))
+    
+    return all_calls_df, all_puts_df
+
+def get_options_chain_expiries(ticker: str='SPY') -> list[datetime]:
+    assert ticker in ['SPY'] # Valid Ticker Assertion
+
+    try:
+        response = requests.get(
+            f'{YFINANCE_URL}/quote/{ticker}/options',
+            headers=HEADERS,
+            timeout=YFINANCE_TIMEOUT,
+        )
+        bsoup = BeautifulSoup(response.content, 'html.parser')
+        return [
+            datetime.strptime(date_str.text, '%B %d, %Y').date() 
+            for date_str in bsoup.find_all('select')[0]
+        ]
+
+    except requests.exceptions.ReadTimeout:
+        return None
+
+if __name__ == '__main__':
+    expiries = get_options_chain_expiries()
+    #print(expiries[0])
+    print(get_options_chain_data(expiries[0])[0]['Quote'])
+    #print(get_options_chain_data_batch(expiries))
